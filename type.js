e326ad4268e634b567598b26b87cd54f80242175
ROSHAN17052 <cpkroshan@gmail.com>
2021-02-08 08:46:30 +0530

    html code

---
+++ type.js
@@ -0,0 +1,11 @@
+
+
+
+let example2 =new Typed("#example2",{
+    strings:["Web Applications","Mobile Application","E-Commerse Websites",
+"Portpolio Websites","Social Media Websites"],
+    typeSpeed:100,
+    backSpeed:100,
+      loop :true,
+      fadeOut:true
+});
